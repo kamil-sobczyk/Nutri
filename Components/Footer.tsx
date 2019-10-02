d91cc5f5962979a611181a5d955da0d10304aaf8
Kamil Sobczyk <kamil.sobczyk@blue-focus.com>
2019-10-02 14:14:07 +0200

    create footer

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,32 +1,65 @@
-import React, { Component } from 'react';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
-export default class FooterTabs extends Component {
+import React, {Component} from "react";
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  FooterTab,
+  Button,
+  Icon,
+  Text
+} from "native-base";
+import {observer, inject} from "mobx-react";
+import {StoreProps} from "../Lib/Store/RootStore";
+import {observable} from "mobx";
+import {bool} from "prop-types";
+import {StyleSheet} from "react-native";
+import {FooterSingleTab} from "../Lib/Store/Stores/ScreenController";
+
+@inject("store")
+@observer
+export default class FooterTabs extends Component<any, any> {
+  private tabs: FooterSingleTab[] = this.props.store.screenController
+    .footerTabs;
+
+  private onTabPress = (index: number) => {
+    this.setActiveTab(index);
+  };
+
+  private setActiveTab = (index: number) => {
+    this.tabs.map((tab: FooterSingleTab, i: number) => {
+      if (index === i) {
+        tab.active = true;
+        tab.onPress();
+      } else {
+        tab.active = false;
+      }
+    });
+  };
+
   render() {
     return (
-      <Container>
-        <Header />
-        <Content />
-        <Footer>
-          <FooterTab>
-            <Button vertical>
-              <Icon name="apps" />
-              <Text>Apps</Text>
+      <Footer style={styles.footer}>
+        <FooterTab>
+          {this.tabs.map((tab: FooterSingleTab, index: number) => (
+            <Button
+              vertical
+              key={tab.icon}
+              onPress={() => this.onTabPress(index)}
+              active={tab.active}
+            >
+              <Icon name={tab.icon} />
             </Button>
-            <Button vertical>
-              <Icon name="camera" />
-              <Text>Camera</Text>
-            </Button>
-            <Button vertical active>
-              <Icon active name="navigate" />
-              <Text>Navigate</Text>
-            </Button>
-            <Button vertical>
-              <Icon name="person" />
-              <Text>Contact</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
+          ))}
+        </FooterTab>
+      </Footer>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  footer: {
+    position: "absolute",
+    bottom: 0
+  }
+});
